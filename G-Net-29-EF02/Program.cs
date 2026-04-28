1f626752892ff5aafbbcc76a1400813056e96081
Abdelrahman Emam <botta12779@gmail.com>
2026-04-28 23:39:06 +0200

    Add project files.

---
+++ G-Net-29-EF02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_Net_29_EF02
+    {
+    internal class Program
+        {
+        static void Main( string[] args )
+            {
+            Console.WriteLine("Hello, World!");
+            }
+        }
+    }
